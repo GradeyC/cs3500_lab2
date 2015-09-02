673d93d7f27a1ff943620973940a8405e740f6f9
Gradey Cullins <gradeycullins@gmail.com>
2015-09-02 10:52:36 -0600

    wrote a string to console, keep console open using Console.Read()

--- repoDemoSolution/repoDemoSolution/Program.cs
+++ repoDemoSolution/repoDemoSolution/Program.cs
@@ -10,6 +10,8 @@ namespace repoDemoSolution
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("my computer is talking wtf");
+            Console.Read();
         }
     }
 }
